9002e8be7e73fe9cd48b86a46fd13022feda6864
Coffee Warrior <lionlikelucas@gmail.com>
2018-08-17 07:16:33 -0400

    added tests for DELETE/:todo path

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -106,3 +106,49 @@ describe("GET /todos/:id", () => {
             .end(done)
     })
 })
+
+describe("DELETE /todos/:id", () => {
+    it("should remove a todo", (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId); 
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((res) => {
+                    expect(res).toNotExist()
+                    done()
+                }).catch(e => done(e))
+            })
+    });
+
+    it("should return 404 if todo not found", (done) => {
+        request(app)
+            .delete(`/todo/${new ObjectID()}`)
+            .expect(404)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+               done()
+            })
+    });
+
+    it("should return 404 if object id is invalid", (done) => {
+        request(app)
+            .delete(`/todo/12345`)
+            .expect(404)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+               done()
+            })
+    })
+})
